bf755b89642821f9321ec7534a3285fb56a6c3c6
seongukHEO <heoseonguk3@gmail.com>
2025-11-03 23:10:54 +0900

    feat : 파베 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:damandu/damandu_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  //await _initializeServices();
+  await _initializeServices();
 
   // 세로 모드만 허용
   await SystemChrome.setPreferredOrientations([
@@ -26,15 +29,15 @@ void main() async {
   // );
 }
 
-// Future<void> _initializeServices() async {
-//   await SharedPreferenceProvider.init();
-//   await dotenv.load(fileName: 'assets/etc/.env');
-//   await initializeDateFormatting(AppLocale.commonLocale, null);
-//
-//   // 🚨 여기 수정
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//
-//   KakaoSdk.init(nativeAppKey: dotenv.get('KAKAO_NATIVE_APP_KEY'));
-// }
+Future<void> _initializeServices() async {
+  // await SharedPreferenceProvider.init();
+  // await dotenv.load(fileName: 'assets/etc/.env');
+  // await initializeDateFormatting(AppLocale.commonLocale, null);
+
+  // 🚨 여기 수정
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //KakaoSdk.init(nativeAppKey: dotenv.get('KAKAO_NATIVE_APP_KEY'));
+}
