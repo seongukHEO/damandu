f79175085113bc2ae0508c00f2ff3b3e1e718fff
seongukHEO <heoseonguk3@gmail.com>
2025-11-17 05:54:55 +0900

    feat : ios 빌드 성공

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps  // ✅ ① Google Maps 임포트 추가
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // ✅ ② Google Maps API 키 등록 (여기 본인 키로 교체)
+    GMSServices.provideAPIKey("AIzaSyB3ohtt1RS5cEawWSDijkoON427n2OvR9g")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
